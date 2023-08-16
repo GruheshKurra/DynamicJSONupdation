307a06916c61e248514e9393166bfac7a7da3f4e
Kurra Gruhesh Sri Sai Karthik <106949584+GruheshKurra@users.noreply.github.com>
2023-08-16 11:23:49 +0530

    Update python.py

--- python.py
+++ python.py
@@ -11,22 +11,43 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 
 # Define SQL statements
+create_table_sql = """
+CREATE TABLE IF NOT EXISTS config (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    environment VARCHAR(255),
+    section VARCHAR(255),
+    `key` VARCHAR(255),
+    value VARCHAR(255),
+    flag INT DEFAULT 0
+);
+"""
 insert_sql = """
-  INSERT INTO config (environment, section, `key`, value)
-  VALUES (%s, %s, %s, %s)  
+INSERT INTO config (environment, section, `key`, value)
+VALUES (%s, %s, %s, %s)  
 """
 update_sql = """
-  UPDATE config 
-  SET value = %s
-  WHERE environment = %s AND section = %s AND `key` = %s
+UPDATE config 
+SET value = %s, flag = 1
+WHERE environment = %s AND section = %s AND `key` = %s
 """
+reset_flags_sql = """
+UPDATE config
+SET flag = 0
+"""
+
+# Create the table if it doesn't exist
+cursor.execute(create_table_sql)
+
+# Reset all flags to 0 before processing updates
+cursor.execute(reset_flags_sql)
+db.commit()
 
 # Load JSON data from file
 with open(r"C:\Users\akagr\Downloads\Prototype.json") as f:
     data = json.load(f)
 
 # Load the existing JSON data from file (json2)
-with open(r"C:\Users\akagr\Downloads\prototypejsontojson.json", "r") as f:
+with open(r"C:\Users\akagr\Downloads\prototype2.json", "r") as f:
     json2 = json.load(f)
 
 # Dictionary to keep track of updated values
@@ -36,15 +57,17 @@ for env, env_config in data.items():
     for section, section_config in env_config.items():
         for key, value in section_config.items():
             # Check if row exists
-            select_sql = "SELECT 1 FROM config WHERE environment = %s AND section = %s AND `key` = %s"
+            select_sql = "SELECT value FROM config WHERE environment = %s AND section = %s AND `key` = %s"
             cursor.execute(select_sql, (env, section, key))
             row = cursor.fetchone()
 
             if row:
-                # Update existing row
-                values = (value, env, section, key)
-                cursor.execute(update_sql, values)
-                updated_values[(env, section, key)] = value
+                # Compare current value with existing value
+                if row[0] != value:
+                    # Update existing row
+                    values = (value, env, section, key)
+                    cursor.execute(update_sql, values)
+                    updated_values[(env, section, key)] = value
             else:
                 # Insert new row
                 values = (env, section, key, value)
@@ -60,7 +83,7 @@ for (env, section, key), value in updated_values.items():
         json2[env][section][key] = value
 
 # Save the updated json2 data back to the file
-with open(r"C:\Users\akagr\Downloads\prototypejsontojson.json", "w") as f:
+with open(r"C:\Users\akagr\Downloads\prototype2.json", "w") as f:
     json.dump(json2, f, indent=4)
 
 print("Database updated and JSON file updated.")
